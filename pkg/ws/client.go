ac79cb3e091c3cbe48eb05fa5e3db0d0aac3bb84
jinquan <jinquan@aithu.com>
2024-01-09 10:57:13 +0000

    add all

--- pkg/ws/client.go
+++ pkg/ws/client.go
@@ -2,6 +2,10 @@ package ws
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"time"
 
 	"github.com/pkg/errors"
@@ -44,8 +48,65 @@ func (c *Client) Read() {
 			break
 		}
 
-		fmt.Println("Read websocket: ", msg)
-		c.Write(1, []byte(msg.Message+" it is response"))
+		fmt.Println("Read websocket: ", msg.Message)
+
+		// cmd := exec.Command("/home/spa", " < ", " in.sphere")
+		// cmd.Dir = "/home/sparta-13Apr2023/bench"
+
+		// // 创建一个Buffer，并将msg.Message的内容写入这个Buffer
+		// var stdin bytes.Buffer
+		// stdin.Write([]byte(msg.Message))
+		// cmd.Stdin = &stdin
+
+		// // 获取输出对象，可以从该对象中读取输出结果
+		// output, err := cmd.Output()
+		// if err != nil {
+		// 	fmt.Println("cmd err: ", err)
+		// }
+
+		cmd := exec.Command("/home/spa")
+		cmd.Dir = "/home/sparta-13Apr2023/bench"
+		// 打开文件
+		file, err := os.Open("/home/sparta-13Apr2023/bench/in.sphere")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		// 将命令的标准输入重定向到文件
+		cmd.Stdin = file
+
+		// 创建一个管道来捕获命令的输出
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// 开始执行命令
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		// 读取命令的输出
+		output, err := ioutil.ReadAll(stdout)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// 等待命令执行完成
+		if err := cmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("The output: %s\n", output)
+
+		// 打印输出结果
+		fmt.Printf("%s\n", output)
+
+		// output 格式化输出
+		result := fmt.Sprintf("%s", output)
+		c.Write(1, []byte(result))
+		c.Write(1, []byte(result))
 	}
 }
 
